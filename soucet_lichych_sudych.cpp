cadf24dad240848820dddd9394197bab2279a56c
maximmat <maximmatous@seznam.cz>
2022-06-20 16:58:11 +0200

    Upload

---
+++ soucet_lichych_sudych.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int soucet_sudych(cislo) {
+    int suda = 0;
+    for (int i = 0; i <= cislo; i += 2) {
+        suda += i;
+    }
+    return suda;
+}
+
+int soucet_lichych(cislo) {
+    int licha = 0;
+    for (int i = 1; i <= cislo; i += 2) {
+        licha += i;
+    }
+    return licha;
+}
+
+int main() {
+    printf("Soucet sudych: %d\n", soucet_sudych(5));
+    printf("Soucet lichych: %d\n", soucet_lichych(5));
+}
